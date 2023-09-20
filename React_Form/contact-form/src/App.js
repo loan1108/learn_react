986c6478a10b5641e6707aa5702862de1dbec316
Loan <you@example.com>
2023-09-20 09:50:17 +0700

    Have done contact form

--- React_Form/contact-form/src/App.js
+++ React_Form/contact-form/src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import "./App.css";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Formik
+      initialValues={{
+        name: "",
+        email: "",
+        phone: "",
+        message: "",
+      }}
+      validate={(values) => {
+        const errors = {};
+        if (!values.name) {
+          errors.name = "Required";
+        }
+        if (!values.email) {
+          errors.email = "Required";
+        } else if (
+          !/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(values.email)
+        ) {
+          errors.email = "Invalid email address";
+        }
+        if (!values.phone) {
+          errors.phone = "Required";
+        }
+        return errors;
+      }}
+      onSubmit={(values) => {
+        if (values) {
+          alert("Submit successfully!!");
+        }
+      }}
+    >
+      {({ errors }) => (
+        <Form>
+          <div className={`custom-input ${errors.name?"custom-input-error":""}`}>
+            <label className="label" htmlFor="name">
+              Name
+            </label>
+            <br />
+            <Field className="custom-input" type="text" name="name" />
+            <ErrorMessage className="error" name="name" component="div" />
+          </div>
+          <div className={`custom-input ${errors.name?"custom-input-error":""}`}>
+            <label className="label" htmlFor="email">
+              Email
+            </label>
+            <br />
+            <Field className="custom-input" type="text" name="email" />
+            <ErrorMessage className="error" name="email" component="div" />
+          </div>
+          <div className={`custom-input ${errors.name?"custom-input-error":""}`}>
+            <label className="label" htmlFor="phone">
+              Phone
+            </label>
+            <br />
+            <Field className="custom-input" type="number" name="phone" />
+            <ErrorMessage className="error" name="phone" component="div" />
+          </div>
+          <div>
+            <label className="label" htmlFor="message">
+              Message
+            </label>
+            <br />
+            <Field className="custom-input" type="textarea" name="message" />
+            <ErrorMessage className="error" name="message" component="div" />
+          </div>
+          <button type="submit">Submit</button>
+        </Form>
+      )}
+    </Formik>
   );
 }
-
-export default App;
