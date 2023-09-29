64e232d13c75d31a5b0ad663e76b7df9e04f558d
Loan <you@example.com>
2023-09-29 15:37:10 +0700

    Doing 1,2 exercise

---
+++ API_client/2_axios/src/App.js
@@ -0,0 +1,25 @@
+import React,{useEffect, useState} from 'react'
+import axios from "axios"
+export default function App() {
+  const [users, setUsers] = useState([])
+  useEffect(()=>{
+    axios.get("http://localhost:3001/api/users")
+    .then(res=>{
+      const newUsers = [...users]//
+      setUsers(res.data)
+    })
+
+  },[])
+  return (
+    <>
+    <div>
+      <h1>Users</h1>
+      <ul>
+        {users&& users.map(user=> (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+    </>
+  )
+}
